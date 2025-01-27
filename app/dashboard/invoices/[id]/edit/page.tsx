b606462fdcd63eea1de4d412d6a0ac54994151e5
jaskirat <jaskiratsingh251103@gmail.com>
2025-01-27 20:23:25 +0530

    edit 4

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,7 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
     
-import NotFound from './not-found';
+
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -11,9 +11,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
       fetchInvoiceById(id),
       fetchCustomers(),
     ]);
-    if (!invoice) {
-        NotFound();
-      }
+  
   return (
     <main>
       <Breadcrumbs
